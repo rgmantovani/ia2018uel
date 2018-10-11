a3b340c740ce2e62b76b5f7679486b5850fe1c32
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2018-10-10 21:45:49 -0300

    adding perceptron skeleton

---
+++ code/perceptronSchema.R
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------------
+# training perceptron in a dataset
+# -----------------------------------------------------------------
+
+perceptron.train = function(dataset, weights, lrn.rate) {
+
+	epochs = 0
+	error  = TRUE
+
+	while(error) {
+
+		# for each example
+
+			# do the sum (v)
+			# calculates the signal function
+			# check if update is necessary
+
+	}
+
+}
+
+# -----------------------------------------------------------------
+# Evaluating new examples after the model was trained
+# -----------------------------------------------------------------
+
+perceptron.test = function(example, weights) {
+
+}
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
